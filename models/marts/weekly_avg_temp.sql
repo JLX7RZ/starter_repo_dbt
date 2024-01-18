b12da330f663783103247b43c58fe2bcb1534e3b
JLX7RZ <benniprz@gmx.de>
2024-01-18 14:32:09 +0100

    gogogoo

--- models/marts/weekly_avg_temp.sql
+++ models/marts/weekly_avg_temp.sql
@@ -1,10 +1,10 @@
 with total_avg as (
     SELECT
-    city, country, year, lat, lon,
+    city, country, lat, lon,
     avg(avgtemp_c) as avg_temp_week,
     max(maxtemp_c) as max_temp_week,
     min(mintemp_c) as mix_temp_week
     from {{ref("prep_temp")}}
-    Group by city, country, year, week, lat, lon
+    Group by city, country, week, lat, lon
     )
 SELECT * from total_avg
